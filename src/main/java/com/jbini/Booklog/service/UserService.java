e056c7750d8612e3313546907581f3bbb823b9fd
JiniEun <julietjin94@gmail.com>
2022-05-10 17:50:52 +0900

    Fix UserService

--- src/main/java/com/jbini/Booklog/service/UserService.java
+++ src/main/java/com/jbini/Booklog/service/UserService.java
@@ -6,8 +6,8 @@ import com.jbini.Booklog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
